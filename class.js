ead23b550af906d5ad901cb26230127b2b43e02e
abiola823 <olaideabiola2@gmail.com>
2023-09-06 15:26:05 +0100

    object oriented programming

---
+++ class.js
@@ -0,0 +1,15 @@
+class bankAccount  {
+    constructor(name, balance, typeOfAccount) {
+        this.balance = balance;
+        this.name = name;
+        this.typeOfAccount = typeOfAccount; 
+    }
+    withdraw(){
+    }
+}
+const abiola = new bankAccount("Olaide", 10000, "studentAccount");
+const dayo = new bankAccount("adedayo", 400, "workingClass");
+
+
+console.log(abiola.name);
+console.log(dayo.name);
